db308974ce7a0eaa2aaf4a79b789d54ce7f68962
Iris Xu <xuiris97@gmail.com>
2018-12-07 12:37:25 -0800

    Add interest functionality implemented

--- addInterest.java
+++ addInterest.java
@@ -1,3 +1,11 @@
+
+import java.sql.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import static java.lang.Math.toIntExact;
+import java.text.*;
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,11 +18,141 @@
  */
 public class addInterest extends javax.swing.JFrame {
 
+    private final Bank bank;
+    private Connection conn;
+    private java.util.Date sysDate;
+    
     /**
      * Creates new form addInterest
      */
     public addInterest() {
         initComponents();
+        bank = new Bank();
+        conn = bank.getConnection();      
+        
+        // check if interest has already been added in the past month.
+        String qry = "SELECT MAX(day) as lastday FROM AccrueInterest";
+        try {
+            DateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+            String d = SystemTime.getSystemDate();
+            sysDate = format.parse(d);
+            
+            Statement stmt = conn.createStatement();
+            ResultSet t = stmt.executeQuery(qry);
+            if (t.next()){
+                java.util.Date lastDay = t.getDate("lastday");
+                if (lastDay == null) {
+                    // No record that interest has ever been added. Perform,
+                    performAddInterest();
+                }
+                else if (diffDays(lastDay, sysDate) > 30) {
+                    // Interest hasn't been accrued in past 30 days.
+                    // Can now do it...
+                    performAddInterest();
+                } else {
+                    status.setText("Interest has already been added this month.");
+                }                   
+            }        
+            t.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error adding interest");
+            status.setText("Error adding interest");
+        } catch (ParseException e) {
+            System.out.println("Issue with parsing system time");
+            status.setText("Issue with parsing system time");
+        } 
+    }
+    
+    public void performAddInterest() {
+        String date = SystemTime.getSystemDate();
+        
+        // get all accounts in bank
+        String qry = "SELECT a.aid, a.interest FROM Accounts a";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet accts = stmt.executeQuery(qry);
+
+            while(accts.next()){
+                int aid  = accts.getInt("aid");
+                double interest = accts.getDouble("interest");
+                double avg = getAvgBalance(aid); // average daily balance
+                System.out.println("Avg daily for " + aid + " is " + avg);
+                
+                Account a = Account.getAccount(conn, aid);
+                a.balance += avg*(interest/100.0);
+                a.updateAccountDB(conn);
+            }
+            accts.close();
+            
+            Transaction.createAccrueInterest(conn, date);
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error adding interest");
+            status.setText("Error adding interest");
+        } 
+        
+        status.setText("Interest added for this month");
+    }
+    
+    public double getAvgBalance(int aid) {
+        
+        // day, amt 
+        ArrayList<Map.Entry<java.util.Date, Double>> daily = new ArrayList<Map.Entry<java.util.Date, Double>>();
+        
+        String qry = "SELECT avg(b.balance) as avgbal, b.day FROM Balances b" 
+                + " WHERE b.aid = " + aid
+                + " GROUP BY day";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet b = stmt.executeQuery(qry);
+
+            while(b.next()){
+                double bal  = b.getDouble("avgbal");
+                java.util.Date d = b.getDate("day");
+                daily.add(new AbstractMap.SimpleEntry<>(d, bal));
+            }
+            
+            // delete his balances to set up for next time.
+            qry = "DELETE FROM Balances b" 
+                + " WHERE b.aid = " + aid;
+            stmt.executeQuery(qry);
+            
+            b.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error getting balances database");
+            status.setText("Error getting balances database");
+        } 
+        
+        double sum = 0.0; 
+        double days = diffDays(sysDate, daily.get(0).getKey());
+        for (int i = 1; i <= daily.size()-1; i++) {
+            // get diff in the days since last balance change
+            java.util.Date last = daily.get(i-1).getKey();
+            int elapsed = diffDays(daily.get(i).getKey(), last); 
+            
+            System.out.println("Was at " + daily.get(i).getValue() + " for " + elapsed + " days.");
+            System.out.println(last + " to " + daily.get(i).getKey());
+            
+            double bal = daily.get(i).getValue();
+            sum += bal * (elapsed/days);
+        }
+        int lastElement = daily.size()-1;
+        int elapsed = diffDays(sysDate, daily.get(lastElement).getKey());
+        double bal = daily.get(lastElement).getValue();
+        System.out.println("Was at " + bal + " for " + elapsed + " days.");
+        System.out.println(sysDate + " to " + daily.get(lastElement).getKey());
+        sum += bal * (elapsed/days);
+        
+        return sum;
+    }
+    
+    public int diffDays(java.util.Date fin, java.util.Date init) {
+        long diffInMillies = fin.getTime() - init.getTime();
+        return toIntExact(TimeUnit.DAYS.convert(diffInMillies,TimeUnit.MILLISECONDS));
+        //return (int)( (fin.getTime() - init.getTime()) / (1000 * 60 * 60 * 24) );
     }
 
     /**
@@ -28,6 +166,7 @@ public class addInterest extends javax.swing.JFrame {
 
         back = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        status = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -40,25 +179,36 @@ public class addInterest extends javax.swing.JFrame {
 
         jLabel1.setText("Add Interest");
 
+        status.setText("Status");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(331, Short.MAX_VALUE)
-                .addComponent(back)
-                .addContainerGap())
             .addGroup(layout.createSequentialGroup()
-                .addGap(170, 170, 170)
-                .addComponent(jLabel1)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(331, Short.MAX_VALUE)
+                        .addComponent(back))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(170, 170, 170)
+                                .addComponent(jLabel1))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(32, 32, 32)
+                                .addComponent(status)))
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(25, 25, 25)
                 .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 227, Short.MAX_VALUE)
+                .addGap(35, 35, 35)
+                .addComponent(status)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 176, Short.MAX_VALUE)
                 .addComponent(back)
                 .addContainerGap())
         );
@@ -110,5 +260,6 @@ public class addInterest extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton back;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel status;
     // End of variables declaration//GEN-END:variables
 }
